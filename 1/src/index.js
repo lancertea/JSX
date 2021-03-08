042ad2ad8d37e75ce6b25faa210478445ab28c9d
lancertea <864798435@qq.com>
2021-03-08 11:42:54 +0800

    clear

--- 1/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './12/App1';
-
-//'./12/App' 传值
-// ReactDOM.render(<App name="lancer" />, document.getElementById('root'));
-
-ReactDOM.render(<App />, document.getElementById('root'));
